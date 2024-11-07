30ecd7156df0ae390ef0d6fc9101eff1da56d0b1
moooazsayyed <sayyedmooaz@gmail.com>
2024-11-08 02:25:13 +0530

    made complete home page and media page

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-
+// import
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
